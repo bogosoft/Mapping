84875ef229aab2a04a3d986c400b9038f8326f57
Clayton Roth <croth0627@yahoo.com>
2017-01-27 01:06:35 -0800

    Build-related changes.

--- Bogosoft.Mapping/Properties/AssemblyInfo.cs
+++ Bogosoft.Mapping/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bogosoft.Mapping")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Data type mapping contracts for .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Clayton Roth")]
 [assembly: AssemblyProduct("Bogosoft.Mapping")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
