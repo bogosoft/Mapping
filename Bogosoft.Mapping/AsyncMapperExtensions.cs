caeefcdba31cea18b164bcdb096190f2e29cc34f
Clayton Roth <croth0627@yahoo.com>
2017-06-28 22:46:33 -0700

    added utility extension methods for delegates

--- Bogosoft.Mapping/AsyncMapperExtensions.cs
+++ Bogosoft.Mapping/AsyncMapperExtensions.cs
@@ -9,6 +9,41 @@ namespace Bogosoft.Mapping
     public static class AsyncMapperExtensions
     {
         /// <summary>
+        /// Map an object of one type to an object of another.
+        /// </summary>
+        /// <typeparam name="TIn">The type of the input object.</typeparam>
+        /// <typeparam name="TOut">The type of the output object.</typeparam>
+        /// <param name="mapper">The current <see cref="AsyncMapper{TIn, TOut}"/> implementation.</param>
+        /// <param name="input">An object of the input type.</param>
+        /// <returns>
+        /// An object of the output type.
+        /// </returns>
+        public static Task<TOut> MapAsync<TIn, TOut>(this AsyncMapper<TIn, TOut> mapper, TIn input)
+        {
+            return mapper.Invoke(input, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Map an object of one type to an object of another.
+        /// </summary>
+        /// <typeparam name="TIn">The type of the input object.</typeparam>
+        /// <typeparam name="TOut">The type of the output object.</typeparam>
+        /// <param name="mapper">The current <see cref="AsyncMapper{TIn, TOut}"/> implementation.</param>
+        /// <param name="input">An object of the input type.</param>
+        /// <param name="token">A <see cref="CancellationToken"/> object.</param>
+        /// <returns>
+        /// An object of the output type.
+        /// </returns>
+        public static Task<TOut> MapAsync<TIn, TOut>(
+            this AsyncMapper<TIn, TOut> mapper,
+            TIn input,
+            CancellationToken token
+            )
+        {
+            return mapper.Invoke(input, token);
+        }
+
+        /// <summary>
         /// Map an object of the input type to an object a given output type. Calling this method is equivalent to calling
         /// <see cref="IMapAsync{TIn}.MapAsync{TOut}(TIn, CancellationToken)"/> with a value of
         /// <see cref="CancellationToken.None"/>.
